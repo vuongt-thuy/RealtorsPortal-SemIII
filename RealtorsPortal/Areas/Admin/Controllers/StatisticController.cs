0e509b92a19739b62245e9cde2d1f1229feb845c
= <=>
2021-08-12 16:07:57 +0700

    admin_statistic

---
+++ RealtorsPortal/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealtorsPortal.Areas.Admin.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Admin/Statistic
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+        public ActionResult Ads()
+        {
+            return View();
+        }
+    }
+}
